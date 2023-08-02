a5f396afcb129bcab3773269bb9fb7ae4e573ebc
okaneco <47607823+okaneco@users.noreply.github.com>
2023-08-02 00:21:48 -0400

    Speed up Lab conversion with Srgb/LinSrgb fast path
    Srgb/LinSrgb conversion between u8 and f32/f64 has been special-cased
    to use a lookup table and approximation. This PR uses that
    implementation where possible.
    
    Speedup gains increase as image size increases. On a 4500x3000 image,
    a 44% speedup was seen reducing the time from 7.8s to 4.3s after
    switching from `into_format` to using `into_linear`. The speedup is
    closer to 15-17% on smaller images (320x200) but still noticeable
    when measured.
    
    Swap to using palette's FromStr implementation for colors. Now 3 or 6
    digit hex colors are permitted.

--- src/bin/kmeans_colors/err.rs
+++ src/bin/kmeans_colors/err.rs
@@ -26,13 +26,11 @@ impl From<std::time::SystemTimeError> for CliError {
 
 impl std::fmt::Display for CliError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match *self {
-            CliError::File(ref err) => write!(f, "File error: {}", err),
-            CliError::InvalidHex => {
-                write!(f, "Error: Invalid hex color length, must be 6 characters.")
-            }
-            CliError::Parse(ref err) => write!(f, "Parse error: {}", err),
-            CliError::Time(ref err) => write!(f, "Time error: {}", err),
+        match self {
+            CliError::File(err) => write!(f, "{err}"),
+            CliError::Parse(err) => write!(f, "{err}"),
+            CliError::Time(err) => write!(f, "{err}"),
+            CliError::InvalidHex => write!(f, "Invalid hex color, must be 3 or 6 digts"),
         }
     }
 }
@@ -41,9 +39,9 @@ impl std::error::Error for CliError {
     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
         match self {
             CliError::File(err) => Some(err),
-            CliError::InvalidHex => None,
             CliError::Parse(err) => Some(err),
             CliError::Time(err) => Some(err),
+            CliError::InvalidHex => None,
         }
     }
 }
